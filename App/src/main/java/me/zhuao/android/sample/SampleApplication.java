c1375f8d03d95ba8add1e4ca83ff2fc951840171
zhuao <zhuao1051@gmail.com>
2016-07-12 08:49:58 +0800

    delet other activity

--- App/src/main/java/me/zhuao/android/sample/SampleApplication.java
+++ App/src/main/java/me/zhuao/android/sample/SampleApplication.java
@@ -3,12 +3,6 @@ package me.zhuao.android.sample;
 import android.app.Application;
 
 import me.zhuao.android.controller.Navigator;
-import me.zhuao.android.sample.activity.AudioRecorderActivity;
-import me.zhuao.android.sample.activity.ButtonActivity;
-import me.zhuao.android.sample.activity.EditTextActivity;
-import me.zhuao.android.sample.activity.MomentsActivity;
-import me.zhuao.android.sample.activity.SpinnerActivity;
-import me.zhuao.android.sample.activity.StepIndicatorActivity;
 import me.zhuao.android.sample.activity.TodoActivity;
 
 public class SampleApplication extends Application {
@@ -17,12 +11,6 @@ public class SampleApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Navigator.init(getApplicationContext());
-        Navigator.map("/spinner", SpinnerActivity.class);
         Navigator.map("/todo", TodoActivity.class);
-        Navigator.map("/button", ButtonActivity.class);
-        Navigator.map("/text", EditTextActivity.class);
-        Navigator.map("/audio", AudioRecorderActivity.class);
-        Navigator.map("/moments", MomentsActivity.class);
-        Navigator.map("/step-indicator", StepIndicatorActivity.class);
     }
 }
